9ddeab2d693d1d39b77d5a0a93375da8d7d1d974
Mason Childers <31513862+Vaposlays@users.noreply.github.com>
2017-11-15 09:15:17 -0700

    initialized data members in ChatPanel

--- src/chat/view/ChatPanel.java
+++ src/chat/view/ChatPanel.java
@@ -1,6 +1,49 @@
 package chat.view;
 
-public class ChatPanel
-{
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.JTextArea;
+import javax.swing.SpringLayout;
+import chat.controller.ChatController;
 
+public class ChatPanel extends JPanel
+{
+	private ChatController appController;
+	private JButton chatButton;
+	private JTextField inputField;
+	private JTextArea chatArea;
+	private SpringLayout appLayout;
+	
+	public ChatPanel(ChatController appController)
+	{
+		super();
+		this.appController = appController;
+		
+		//Initialize GUI data members
+		chatButton = new JButton("chat");
+		chatArea = new JTextArea(10,25);
+		inputField = new JTextField(20);
+		appLayout = new SpringLayout();
+		
+		setupPanel();
+		setupLayout();
+		setupListeners();
+	}
+	
+	private void setupPanel()
+	{
+		
+	}
+	
+	private void setupLayout()
+	{
+		
+	}
+	
+	private void setupListeners()
+	{
+		
+	}
+	
 }
