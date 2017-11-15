ab7a920a365a25365cf71bd3f9aaa7f2e16c444a
Mason Childers <31513862+Vaposlays@users.noreply.github.com>
2017-11-15 08:52:21 -0700

    update gui

--- src/chat/view/ChatFrame.java
+++ src/chat/view/ChatFrame.java
@@ -1,22 +1,28 @@
 package chat.view;
 
-import chat.controller.ChatController;
 import javax.swing.JFrame;
+import chat.controller.ChatController;
 
 public class ChatFrame extends JFrame
 {
 	private ChatController appController;
+//	private ChatPanel appPanel;
 
 	public ChatFrame(ChatController appController)
 	{
 		super();
 		this.appController = appController;
+//		appPanel = new ChatPanel(appController);
 		
 		setupFrame();
 	}
 	
 	private void setupFrame()
 	{
+		this.setSize(500,500);
+		this.setTitle("Chatbot 2017");
+//		this.setCotnentPane(appPanel);
+		this.setResizable(false);
 		this.setVisible(true);
 	}
 }
